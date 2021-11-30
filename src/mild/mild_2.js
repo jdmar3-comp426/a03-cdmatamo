0c18b907c7ecb3907c2ff5b508b96af2b179ca72
cdmatamo <cdmatamo@live.unc.edu>
2021-11-30 17:09:00 -0500

    removekeynondestructive 2

--- src/mild/mild_2.js
+++ src/mild/mild_2.js
@@ -71,8 +71,8 @@ export function removeKey(object, key) {
  */
 export function removeKeyNonDestructive(object, key) {
    let new_obj = {};
-   let keys = object.keys();
-   let values = object.values();
+   let keys = Object.keys(object);
+   let values = Object.values(object);
    for (let i = 0; i < keys.length; i++) {
       if (keys[i] != key ) {
          new_obj[keys[i]] = values[i];
