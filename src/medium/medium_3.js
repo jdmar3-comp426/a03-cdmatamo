eff18bf67de4ad141b1bc0fa43092dcc2c21993e
cdmatamo <cdmatamo@live.unc.edu>
2021-12-02 14:12:25 -0500

    medium 3 attempt 2

--- src/medium/medium_3.js
+++ src/medium/medium_3.js
@@ -20,7 +20,7 @@ queries.
 export function searchHighPower(car_data, minHorsepower, minTorque) {
 let highPower = [];
     for (let i = 0; i < car_data.length; i++) {
-        if (car_data[i].horsePower >= minHorsepower && car_data[i].torque >= minTorque) {
+        if (car_data[i].horsepower >= minHorsepower && car_data[i].torque >= minTorque) {
            highPower.push(car_data[i]);
         }
     }
